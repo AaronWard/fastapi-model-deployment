dd9f95473b0af2f166658e617a244a7330597713
Ward <award40@LAMU02WH4RCHTD6.uhc.com>
2022-05-02 11:17:12 +0100

    ADD - tests for both model outcomes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,4 @@
+# pylint: disable=redefined-outer-name
 """
 This file is to test the robustness of the API
 
@@ -35,26 +36,52 @@ def test_incorrect_path(client):
     assert res.json() == {"detail":"Not Found"}
 
 
-def test_post(client):
+def test_post_above(client):
+    """Test for salary above 50K"""
+
     res = client.post("/model", json={
                     "age": 55,
                     "workclass": "Private",
                     "fnlgt": 77516,
                     "education": "Masters",
-                    "education-num": 13,
+                    "education-num": 16,
                     "marital-status": "Never-married",
                     "occupation": "Adm-clerical",
                     "relationship": "Not-in-family",
                     "race": "White",
                     "sex": "Female",
-                    "capital-gain": 2174,
-                    "capital-loss": 0,
-                    "hours-per-week": 40,
+                    "capital-gain": 10000,
+                    "capital-loss": 400,
+                    "hours-per-week": 45,
                     "native-country": "United-States",
             })
 
     assert res.status_code == 200
-    assert res.json() == {"Result": 0}
+    assert res.json() == {'Result': '>50K'}
+
+
+def test_post_below(client):
+    """Test for salary below 50K"""
+    res = client.post("/model", json={
+                    "age": 18,
+                    "workclass": "Self-emp-not-inc",
+                    "fnlgt": 77516,
+                    "education": "HS-grad",
+                    "education-num": 2,
+                    "marital-status": "Never-married",
+                    "occupation": "Adm-clerical",
+                    "relationship": "Not-in-family",
+                    "race": "White",
+                    "sex": "Female",
+                    "capital-gain": 50,
+                    "capital-loss": 10,
+                    "hours-per-week": 30,
+                    "native-country": "United-States",
+            })
+
+    assert res.status_code == 200
+    assert res.json() == {'Result': '<=50K'}
+
 
 def test_broken_post(client):
     res = client.post("/model", json={
